3d7c79c1630dd363a66034d1705f09b5cdb91dc8
mrtnsimeonov <martin.simeonov.highschool@buditel.bg>
2025-04-24 00:53:35 +0300

    Buttons added for different regions

---
+++ TaekwondoRanking/Controllers/RegionsController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TaekwondoRanking.Controllers
+{
+    public class RegionsController : Controller
+    {
+        public IActionResult Bulgaria()
+        {
+            return View();
+        }
+
+        public IActionResult Europe()
+        {
+            return View();
+        }
+
+        public IActionResult World()
+        {
+            return View();
+        }
+    }
+}
---
+++ TaekwondoRanking/Models/RegionPages/AllRegionsViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace TaekwondoRanking.Models.RegionPages
+{
+    public class AllRegionsViewModel
+    {
+        public IEnumerable<AllRegionsViewModel> AllRegions { get; set; }
+        = new List<AllRegionsViewModel>();
+    }
+}

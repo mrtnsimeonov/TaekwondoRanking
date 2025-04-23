d2b78d48f06182178bb4238da74f87ebab4a1fd1
mrtnsimeonov <martin.simeonov.highschool@buditel.bg>
2025-04-24 01:09:36 +0300

    DropDown50%

--- TaekwondoRanking/Controllers/HomeController.cs
+++ TaekwondoRanking/Controllers/HomeController.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using System.Diagnostics;
 using TaekwondoRanking.Models;
+using TaekwondoRanking.Views;
 
 namespace TaekwondoRanking.Controllers
 {
@@ -13,7 +15,34 @@ namespace TaekwondoRanking.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
-        public IActionResult Bulgaria() => View("~/Views/Regions/Bulgaria.cshtml");
+        public IActionResult Bulgaria()
+        {
+            // Mock data for dropdown; replace with database query
+            var categories = new List<SelectListItem>
+        {
+            new SelectListItem { Value = "Junior", Text = "Junior Division" },
+            new SelectListItem { Value = "Cadet", Text = "Cadet Division" },
+            new SelectListItem { Value = "CadetHeight", Text = "Cadet Height Division" }
+        };
+
+            var model = new FilterViewModel
+            {
+                Categories = categories
+            };
+
+            return View(model);
+        }
+
+        [HttpPost]
+        public IActionResult Bulgaria(FilterViewModel model)
+        {
+            // Filter data based on model.SelectedCategory
+            // Example: Query database with the selected category
+            var filteredResults = $"You selected: {model.SelectedCategory}";
+
+            ViewBag.Message = filteredResults;
+            return View(model);
+        }
 
         public IActionResult Europe() => View("~/Views/Regions/Europe.cshtml");
 
---
+++ TaekwondoRanking/Views/FilterViewModel.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace TaekwondoRanking.Views
+{
+    public class FilterViewModel
+    {
+        public string SelectedCategory { get; set; }
+        public List<SelectListItem> Categories { get; set; }
+    }
+}

cd41c1298cd8d512dff31a2048dca82cbf1a131b
mrtnsimeonov <martin.simeonov.highschool@buditel.bg>
2025-05-01 00:50:12 +0300

    Added dropdown filter menus for AgeClass, Gender and Category that filters the list of results

--- TaekwondoRanking/Controllers/RegionsController.cs
+++ TaekwondoRanking/Controllers/RegionsController.cs
@@ -16,22 +16,75 @@ namespace TaekwondoRanking.Controllers
 
         public async Task<IActionResult> World()
         {
-            var athletes = await _context.Athletes
-                .Include(a => a.Results)
-                .Select(a => new AthletePointsViewModel
-                {
-                    IdAthlete = a.IdAthlete,
-                    Name = a.Name,
-                    Country = a.Country,
-                    TotalPoints = a.Results.Sum(r => (int?)r.Points) ?? 0
+            var ageClasses = await _context.Categories.Select(c => c.AgeClass).Distinct().ToListAsync();
+            var genders = await _context.Categories.Select(c => c.Mf).Distinct().ToListAsync();
 
-                })
-                .OrderByDescending(a => a.TotalPoints)
+            var model = new WorldRankingFilterViewModel
+            {
+                AgeClasses = ageClasses,
+                Genders = genders,
+                Categories = new List<string>() // initially empty
+            };
+
+            return View(model);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> World(WorldRankingFilterViewModel model)
+        {
+            var categoriesQuery = _context.Categories.AsQueryable();
+
+            if (!string.IsNullOrEmpty(model.SelectedAgeClass))
+                categoriesQuery = categoriesQuery.Where(c => c.AgeClass == model.SelectedAgeClass);
+
+            if (!string.IsNullOrEmpty(model.SelectedGender))
+                categoriesQuery = categoriesQuery.Where(c => c.Mf == model.SelectedGender);
+
+            if (!string.IsNullOrEmpty(model.SelectedCategory))
+                categoriesQuery = categoriesQuery.Where(c => c.NameCategory == model.SelectedCategory);
+
+            var filteredCategoryIds = await categoriesQuery.Select(c => c.IdCategory).ToListAsync();
+
+            var athletes = await _context.Results
+    .Include(r => r.IdAthleteNavigation)
+        .ThenInclude(a => a.CountryNavigation)
+    .Include(r => r.IdSubCompetition2Navigation)
+        .ThenInclude(sc2 => sc2.IdCategoryNavigation)
+    .Where(r =>
+        r.IdSubCompetition2Navigation.IdCategoryNavigation.AgeClass == model.SelectedAgeClass &&
+        r.IdSubCompetition2Navigation.IdCategoryNavigation.Mf == model.SelectedGender &&
+        r.IdSubCompetition2Navigation.IdCategoryNavigation.NameCategory == model.SelectedCategory)
+    .GroupBy(r => new
+    {
+        r.IdAthleteNavigation.IdAthlete,
+        r.IdAthleteNavigation.Name,
+        Country = r.IdAthleteNavigation.CountryNavigation.NameCountry
+    })
+    .Select(g => new AthletePointsViewModel
+    {
+        IdAthlete = g.Key.IdAthlete,
+        Name = g.Key.Name,
+        Country = g.Key.Country,
+        TotalPoints = g.Sum(x => (int?)x.Points) ?? 0
+    })
+    .OrderByDescending(a => a.TotalPoints)
+    .ToListAsync();
+
+
+
+            model.AgeClasses = await _context.Categories.Select(c => c.AgeClass).Distinct().ToListAsync();
+            model.Genders = await _context.Categories.Select(c => c.Mf).Distinct().ToListAsync();
+            model.Categories = await _context.Categories
+                .Where(c => c.AgeClass == model.SelectedAgeClass && c.Mf == model.SelectedGender)
+                .Select(c => c.NameCategory)
+                .Distinct()
                 .ToListAsync();
 
-            ViewBag.Message = $"Loaded {athletes.Count} athletes with points.";
-            return View(athletes);
+            model.Results = athletes;
+
+            return View(model);
         }
+
 
 
         public IActionResult Bulgaria()
---
+++ TaekwondoRanking/ViewModels/WorldRankingFilterViewModel.cs
@@ -0,0 +1,16 @@
+﻿namespace TaekwondoRanking.ViewModels
+{
+    public class WorldRankingFilterViewModel
+    {
+        public string SelectedAgeClass { get; set; }
+        public string SelectedGender { get; set; }
+        public string SelectedCategory { get; set; }
+
+        public List<string> AgeClasses { get; set; }
+        public List<string> Genders { get; set; }
+        public List<string> Categories { get; set; }
+
+        public List<AthletePointsViewModel> Results { get; set; } = new();
+    }
+
+}
